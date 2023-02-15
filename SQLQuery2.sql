58abde016ba71a89a813f086bffd77f06a6e4c26
Zelv1 <zelenkevichivan950@gmail.com>
2023-02-15 16:19:38 +0300

    Test commit

---
+++ SQLQuery2.sql
@@ -0,0 +1,14 @@
+DELETE FROM [Library].[dbo].[Catalog] WHERE ID = 2;
+
+INSERT INTO [Library].[dbo].[Catalog] (Name, Author, Genre, Cost)
+VALUES ('Три поросёнка', 'Сергей михалков', 'Комедия', 9.600) 
+
+UPDATE [Library].[dbo].[Catalog] set Name = 'Мертвые души', Author = 'Николай Гоголь' WHERE ID = 3;
+
+SELECT TOP (1000) [ID]
+      ,[Name]
+      ,[Author]
+      ,[Genre]
+      ,[Cost]
+  FROM [Library].[dbo].[Catalog]
+  ORDER BY Name
