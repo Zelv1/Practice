ef6d5f8bdc784ac5f53c342521cd586d0a452fff
Zelv1 <zelenkevichivan950@gmail.com>
2023-02-15 17:42:10 +0300

    fix encoding try 5

---
+++ SQLQuery1.sql
@@ -0,0 +1,14 @@
+﻿DELETE FROM [Library].[dbo].[Catalog] WHERE ID = 2;
+
+INSERT INTO [Library].[dbo].[Catalog] (Name, Author, Genre, Cost)
+VALUES (N'Три поросёнка', N'Сергей михалков', N'Комедия', 9.600) 
+
+UPDATE [Library].[dbo].[Catalog] set Name = N'Мертвые души', Author = N'Николай Гоголь' WHERE ID = 3;
+
+SELECT TOP (1000) [ID]
+      ,[Name]
+      ,[Author]
+      ,[Genre]
+      ,[Cost]
+  FROM Library.dbo.Catalog
+  
