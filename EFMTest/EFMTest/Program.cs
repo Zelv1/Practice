9d6eb7c0e3be8150206762238a00dcd255bfcc3d
Zelv1 <zelenkevichivan950@gmail.com>
2023-02-20 16:16:44 +0300

    Entity FraneWork Practice #1

---
+++ EFMTest/EFMTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using EFMTest.Properties;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFMTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (IDbConnection connection = new SqlConnection(Settings.Default.DbConnect))
+            {
+                IDbCommand command = new SqlCommand("SELECT * FROM PersonAndBook");
+                command.Connection = connection;
+
+                connection.Open();
+
+                IDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine("ID_Person: {0}\tID_Book: {1} ", reader.GetValue(0).ToString(), reader.GetValue(1).ToString());
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
